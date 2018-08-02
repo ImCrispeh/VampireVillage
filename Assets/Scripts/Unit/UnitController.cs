a5ec19e3df5edf1fb660f019ff8e4d2f7246b9d8
Christopher Fung <chrisfung2897@hotmail.com>
2018-08-02 11:48:51 +1000

    As a player I want to view the map in a top-down perspective so that I can clearly see what is going on

---
+++ Assets/Scripts/Unit/UnitController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitController : MonoBehaviour {
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit)) {
+                agent.destination = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            }
+        }
+	}
+
+}
