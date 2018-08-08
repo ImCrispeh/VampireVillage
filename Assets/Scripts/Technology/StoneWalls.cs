317b581b3389bd937085bf43299a0e26128529a5
Laptop/Conant <conant.feng@student.uts.edu.au>
2018-08-08 14:57:45 +1000

    Implemented 80% of: As a player I want there to a tech tree so that I can unlock new things and customise my play style

---
+++ Assets/Scripts/Technology/StoneWalls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class StoneWalls : Technology {
+
+    public Image unresearchedImage;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        technologyName = "Stone Walls";
+        technologyDescription = "A stone wall is constructed adding to your defenses";
+        researchCost = 20f; //This will need to be changed once we discuss resources
+        researchTime = 30f; //30 secs - currently not linked to the timer
+        researchTimer = researchTime;
+        researched = false;
+        researching = false;
+        applyTechnology = false;
+        technologyImage = unresearchedImage;
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+        if (researched && !researching && !applyTechnology) {
+            EndResearch();
+            applyTechnology = true;
+        }
+	}
+
+    public override void TechnologyEffect() {
+        //The effects of the technology which are active once research ends
+        //something like Town.Defense += 15;
+        Debug.Log("Added " + technologyName + " to the town");
+    }
+
+    public override void StartResearch() {
+        if (!researched && !researching) {
+            researchTimer = 0;
+            researching = true;
+            Debug.Log("Researching: " + technologyName);
+        }
+    }
+
+    public override void EndResearch() {
+        TechnologyEffect();
+        Debug.Log("Finished researching: " + technologyName);
+    }
+}
---
+++ Assets/Scripts/Technology/WoodenFence.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WoodenFence : Technology {
+
+    public Image unresearchedImage;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        technologyName = "Wooden Fence";
+        technologyDescription = "A wooden fence is constructed adding to your defenses";
+        researchCost = 20f; //This will need to be changed once we discuss resources
+        researchTime = 15f; //15 secs - currently not linked to the timer
+        researchTimer = researchTime;
+        researched = false;
+        researching = false;
+        applyTechnology = false;
+        technologyImage = unresearchedImage;
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+        if (researched && !researching && !applyTechnology) {
+            EndResearch();
+            applyTechnology = true;
+        }
+	}
+
+    public override void TechnologyEffect() {
+        //The effects of the technology which are active once research ends
+        //something like Town.Defense += 5;
+        Debug.Log("Added " + technologyName + " to the town");
+    }
+
+    public override void StartResearch() {
+        if (!researched && !researching) {
+            researchTimer = 0;
+            researching = true;
+            Debug.Log("Researching: " + technologyName);
+        }
+    }
+
+    public override void EndResearch() {
+        TechnologyEffect();
+        Debug.Log("Researching: " + technologyName);
+    }
+}
