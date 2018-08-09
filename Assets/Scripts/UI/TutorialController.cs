7169fe5f3b28f1013e649636a2068713c4492c19
Christopher Fung <chrisfung2897@hotmail.com>
2018-08-09 11:22:41 +1000

    As a player I want there to be a tutorial-like introduction so that I can more easily understand the game
    Continuing on adding tutorial prompts

---
+++ Assets/Scripts/UI/TutorialController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+    public static TutorialController _instance;
+    public int currText;
+    public bool isPaused;
+    public GameObject[] tutorialTexts;
+
+    private void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        isPaused = true;
+        Timer._instance.PauseTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeText() {
+        tutorialTexts[currText].SetActive(false);
+
+        if (currText < tutorialTexts.Length - 1) {
+            currText++;
+            tutorialTexts[currText].SetActive(true);
+        }
+    }
+}
