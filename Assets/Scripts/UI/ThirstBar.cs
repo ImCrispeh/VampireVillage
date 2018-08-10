2aff093bc85a095ffb6e1f358577fcc94b9e2398
Hao\Hao <soccer_is_rad@hotmail.com>
2018-08-10 19:19:38 +1000

    As a player I want a visual indicator so that I can see my hunger level and manage it.

---
+++ Assets/Scripts/UI/ThirstBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThirstBar : MonoBehaviour {
+
+    private float maxThirst;
+    private float currentThirst;
+
+    public Slider thirstBar;
+
+	// Use this for initialization
+	void Start () {
+        maxThirst = 100f;
+        currentThirst = maxThirst;
+
+        thirstBar.value = thirstPercentage();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        drainThirst(0.02f);
+	}
+
+    void drainThirst(float drainAmount)
+    {
+        currentThirst -= drainAmount;
+        thirstBar.value = thirstPercentage();
+    }
+
+    void replenishThirst(float replenishAmount)
+    {
+        currentThirst += replenishAmount;
+        thirstBar.value = thirstPercentage();
+    }
+
+    float thirstPercentage()
+    {
+        return currentThirst / maxThirst;
+    }
+}
