49f8c13192b72d1db23af2dbd7549c4d770eaab9
Hao\Hao <soccer_is_rad@hotmail.com>
2018-08-18 20:13:38 +1000

    As a player I want to be be able to build structures around my base so that I can better defend against threats

--- Assets/Scripts/Technologies/Science/ElixirOfLife.cs
+++ Assets/Scripts/Technologies/Science/ElixirOfLife.cs
@@ -4,29 +4,32 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class SoftMantle : Technology, IPointerEnterHandler, IPointerExitHandler
+public class ElixirOfLife : Technology, IPointerEnterHandler, IPointerExitHandler
 {
 
     public Image unresearchedImage;
     public Image connectingBar;
+    public Image connectingBar2;
     public GameObject technologyObject;
     public Transform technologyPosition;
+    public Technology requiredTechnology;   //add more if you need more than one pre-requiste
 
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
-        technologyName = "Soft Mantle";
+        technologyName = "Elixer of Life";
         technologyDescription = "A soft cloth which lets you perform actions during the day but you still take damage heavily";
-        researchRequirement = "";
-        researchCost = 50;
-        researchTime = 5f;
+        researchRequirement = "Philospher Stone";
+        researchCost = 1000;
+        researchTime = 300f;
         researchTimer = researchTime;
         researched = false;
         researching = false;
         applyTechnology = false;
         technologyImage = unresearchedImage;
         proceedingTechnologyBar.Add(connectingBar);
+        proceedingTechnologyBar.Add(connectingBar2);
         mainBase = BaseController._instance;
     }
 
@@ -51,7 +54,7 @@ public class SoftMantle : Technology, IPointerEnterHandler, IPointerExitHandler
 
     public override void StartResearch()
     {
-        if (!researched && !researching)
+        if (!researched && !researching && requiredTechnology.researched)
         {
             researchTimer = 0;
             researching = true;
@@ -69,7 +72,7 @@ public class SoftMantle : Technology, IPointerEnterHandler, IPointerExitHandler
     {
         Debug.Log("Mouse has entered " + technologyName);
         ttbName.text = technologyName;
-        ttbResearchRequirement.text = researchRequirement;
+        ttbResearchRequirement.text = "Requirement: " + researchRequirement;
         ttbDescription.text = technologyDescription;
         ttbCost.text = researchCost.ToString() + " wood";
         ttbResearchTime.text = researchTime.ToString() + " seconds (need to edit)";
@@ -85,3 +88,4 @@ public class SoftMantle : Technology, IPointerEnterHandler, IPointerExitHandler
         ttbResearchTime.text = "";
     }
 }
+
