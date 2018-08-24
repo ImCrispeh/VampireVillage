5f665abb4d66c4bd371ed37059049b715e681dc9
Hao <haoyin97@gmail.com>
2018-08-24 17:44:37 +1000

    As a player I want to be be able to build structures around my base so that I can better defend against threats

---
+++ Assets/Scripts/Base/BuildingTemplate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTemplate : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject finalBuilding;
+
+    private Vector3 mousePosition;
+
+    [SerializeField]
+    private LayerMask tilesLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(Mathf.Round(mousePosition.x), -10, Mathf.Round(mousePosition.z));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouseRay = Camera.main.ScreenToWorldPoint(transform.position);
+            RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector3.zero, Mathf.Infinity, tilesLayer);
+
+            if (rayHit.collider == null)
+            {
+                Instantiate(finalBuilding, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Base/PlacementController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementController : MonoBehaviour {
+
+    private int selectedBuilding;
+    private GameObject currentBuilding;
+
+    [SerializeField]
+    private GameObject[] selectableBuildings;
+
+    private bool isSelected = false;
+
+
+    // Use this for initialization
+    void Start () {
+        selectedBuilding = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 spawnPosition = new Vector3(Mathf.Round(mousePosition.x), Mathf.Round(mousePosition.y), Mathf.Round(mousePosition.z));
+
+        if (Input.GetKeyDown("q") && isSelected == false)
+        {
+            currentBuilding = (GameObject)Instantiate(selectableBuildings[selectedBuilding], spawnPosition, Quaternion.identity);
+            isSelected = true;
+        }
+
+        //if right button is pressed, destroy the building template on the screen
+        if (Input.GetMouseButtonDown(1) && isSelected == true)
+        {
+            Destroy(currentBuilding);
+            isSelected = false;
+            selectedBuilding = 0; 
+        }
+    }
+
+    void SetBuildingInArray(int orderNumber)
+    {
+        selectedBuilding = orderNumber;
+    }
+}
