d3cd36dfa6ba9930797cbd20951311c574aa8bfd
Laptop/Conant <conant.feng@student.uts.edu.au>
2018-08-27 21:13:46 +1000

    Implemented 50% of: As a player I want a health bar above my town so I can see its hp at a glance (2 hrs)

---
+++ Assets/Scripts/UI/BaseHealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BaseHealthBar : MonoBehaviour {
+    public BaseController _base;
+    public Image healthBar;
+
+	void Start () {
+        _base = this.GetComponent<BaseController>();
+        healthBar = GameObject.Find("MiniCanvas/HealthBar").GetComponent<Image>();
+    }
+	
+	void Update () {
+        UpdateHealthBar();
+	}
+
+    public void UpdateHealthBar() {
+        float currentHealth = _base.health;
+        currentHealth = currentHealth / _base.maxHealth;
+        healthBar.fillAmount = currentHealth;
+    }
+}
