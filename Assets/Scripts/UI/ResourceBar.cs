4911de10f79833ec181ee7294b3eb29d7139cc64
Desktop/Conant <>
2018-08-29 19:58:15 +1000

    Implemented: As a player I want a resource bar so I can see how many resources I have at a glance (1 hr)

---
+++ Assets/Scripts/UI/ResourceBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ResourceBar : MonoBehaviour {
+    public ResourceStorage resources;
+    public Text woodT, stoneT, goldT;
+
+	void Start () {
+        resources = ResourceStorage._instance;
+        woodT = GameObject.Find("ResourceBar/Wood").GetComponent<Text>();
+        stoneT = GameObject.Find("ResourceBar/Stone").GetComponent<Text>();
+        goldT = GameObject.Find("ResourceBar/Gold").GetComponent<Text>();
+    }
+	
+	void Update () {
+        UpdateResources();
+	}
+
+    public void UpdateResources() {
+        woodT.text = resources.wood.ToString();
+        stoneT.text = resources.stone.ToString();
+        goldT.text = resources.gold.ToString();
+    }
+}
