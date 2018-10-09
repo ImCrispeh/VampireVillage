0037303f7db334e910c43dd9162d2feb185d4394
Desktop/Conant <>
2018-10-09 13:47:05 +1100

    Paused game when instructions were opened so players can read in: As a player I want there to be a tutorial-like introduction so that I can more easily understand the game

--- Assets/InstructionsController.cs
+++ Assets/InstructionsController.cs
@@ -6,6 +6,24 @@ public class InstructionsController : MonoBehaviour {
     public GameObject instructions;
 	
     public void ToggleInstructions() {
+        if (SelectionController._instance.selectedObj != null) {
+            SelectionController._instance.DeselectObj();
+            SelectionController._instance.selectedObj = null;
+            SelectionController._instance.repairActionsContainer.SetActive(false);
+            SelectionController._instance.resourceActionBtn.gameObject.SetActive(false);
+            SelectionController._instance.townActionsContainer.SetActive(false);
+            SelectionController._instance.SetObjText();
+            SelectionController._instance.SetObjPortrait();
+        }
+
         instructions.SetActive(!instructions.activeInHierarchy);
+        if (instructions.activeInHierarchy) {
+            SceneController.Instance.togglePseudoPause = true;
+            Timer._instance.PauseTimer();
+        }
+        else {
+            SceneController.Instance.togglePseudoPause = false;
+            Timer._instance.UnpauseTimer();
+        }
     }
 }
